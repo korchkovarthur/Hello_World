a57b4fc0a1ac4abc188e898b41dc4c43312f215f
Arthur Korchkov <qaeng16@gmail.com>
2020-11-12 18:07:42 +0000

    Code

---
+++ Code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*   ********
+       ...
+     ********
+     draw a 8x8 square nested for */
+
+int main(){
+for (int row = 0; row < 8; row++) cout << row << endl;
+}
